a6d6b324650d1b4252e8feadaad31540fdeb295d
Cameron Fensome <126360979+jomam23@users.noreply.github.com>
2024-07-03 15:05:17 -0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,24 @@
+one = input("What is your favorite sleeping position of all time?")
+f = open("1.txt", "w")
+f.write(one)
+f.close()
+
+two = input("Are you going to be home alone for a while?")
+f = open("2.txt", "w")
+f.write(two)
+f.close()
+
+three = input("Hey, quick question. Is it true they’re several species of lizards that squirt out blood from their eyes for a defence mechanism?")
+f = open("3.txt", "w")
+f.write(three)
+f.close()
+
+four = input("What TV channel doesn't exist but really should?")
+f = open("4.txt", "w")
+f.write(four)
+f.close()
+
+five = input("What would a mirror opposite of yourself be like?")
+f = open("5.txt", "w")
+f.write(five)
+f.close()
